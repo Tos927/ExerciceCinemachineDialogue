547d202725e95374bbd8ec2882e499d059cfbe86
Tos927 <t.sautereau92@gmail.com>
2023-01-10 11:05:06 +0100

    test

---
+++ ExerciceCinemachineDialogue/Assets/Script/IntroManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject boat;
+
+    public void RevealMenu()
+    {
+        boat.SetActive(true);
+    }
+}
